f1c45b11c1738f76deb19a9db37b608c405a9ec3
Ahmed Magdy <79047391+ahmedelsangek@users.noreply.github.com>
2021-09-08 22:29:19 +0200

    OOP Concept

--- OOP-With-PHP/index.php
+++ OOP-With-PHP/index.php
@@ -8,35 +8,94 @@
 
         //run when bject created from class
         //it uses ti initialize class's values.
-        function __Construct(string $name, int $age){
+        public function __Construct(string $name, int $age){
             $this -> name = $name;
             $this -> age = $age;
         }
 
         //Setter fucntion
-        function setName($value){
+        //It unused code in the presence of intilize data in constructor
+        public function setName($value){
             $this -> name = $value;
         }
 
         //Getter Function
-        function getName(){
+        public function getName(){
             return $this -> name;
         }
 
-        function print(){
+        public function print(){
             echo "My Name Is " . $this -> name . "and Age Is " . $this -> age;
         }
 
         //it runs in the end of the class
-        function __Destruct(){
-            echo "Hello From Destructor";
+        // function __Destruct(){
+        //     echo "Hello From Destructor";
+        // }
+    }
+
+
+
+    class SubUser extends User
+    {
+        public string $email;
+
+        public function __Construct(string $email, string $name, int $age)
+        {
+            $this -> email = $email;
+            
+            User::__Construct($name, $age);
+        }
+
+        //override function
+        public function print()
+        {
+            echo "My Name Is " . $this -> name . "and Age Is " . $this -> age . "and my email is" . $this -> email;
         }
     }
 
-    $obj = new User("Ahmed Magdy", 25);
-    $obj -> print();
+
+
+    // $parentObj = new User( "Ahmed Magdy", 25);
+    // $childObj = new SubUser( "ahmed@yahoo.com", "Ahmed Magdy", 25);
+
+    // echo $parentObj -> print();
+    // echo "<br>";
+    // echo $childObj -> print();
+
+
+
+
+    // $obj = new User("Ahmed Magdy", 25);
+    // $obj -> print();
     // $obj -> setName("Ahmed Magdy");
     // echo $obj -> getName();
+
+
+
+
+    //Access Modifieries
+    class Book
+    {
+        private string $title;
+
+        public function __Construct(string $title){
+            $this->title = $title;
+        }
+
+        // public function Set($value){
+        //     $this -> title = $value;
+        // }
+
+        public function Get(){
+            return $this -> title;
+        }
+
+    }
+
+    $bookObj = new Book("CS");
+    // $bookObj -> Set();
+    echo $bookObj -> Get();
 
 
 ?>
